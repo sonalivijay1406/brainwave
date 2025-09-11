2098d97ecca1ecd97b2a2f7a3dc92e4a34f91c98
Sonali <sonalivijayvergiya@SONALIs-MacBook-Air.local>
2025-09-11 14:54:59 +0530

    complete

--- src/Roadmap.jsx
+++ src/Roadmap.jsx
@@ -53,7 +53,7 @@ const Roadmap = () => {
                         </div>
                     )
                 })}
-                <Gradient/>
+                
             </div>
             <div className='flex justify-center mt-12 md:mt-15 xl:mt-20'>
                 <Button href="/roadmap">Our roadmap</Button>
